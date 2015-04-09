6a8ce84c09e0e580a900d9ab8b0a0d00835ab0c8
arakoh <arakoh92@gmail.com>
2015-04-09 15:57:38 +0900

    no message

---
+++ plot2.R
@@ -0,0 +1,12 @@
+
+setwd("C:/")
+
+  source("./data/Load data.R")
+ png(filename = "plot2.png",  
+      width = 480, height = 480, 
+      units = "px", bg = "transparent") 
+ plot(DateTime, Global_active_power,  
+      type = "l", 
+      xlab = "", 
+      ylab = "Global Active Power (kilowatts)") 
+ dev.off() 
